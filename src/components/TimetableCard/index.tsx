1ebd7a1176c16bdeae35c94a7f032caef6cbad39
dvtdat <tiendat0417@gmail.com>
2024-02-06 22:25:08 +0700

    chore: adjustment on visualization

--- src/components/TimetableCard/index.tsx
+++ src/components/TimetableCard/index.tsx
@@ -21,7 +21,7 @@ const TimetableCard = ({ startDate, endDate, location }: TimetableCardProps) => 
   });
 
   return (
-    <div className='relative flex min-h-[15rem] w-full flex-col items-start space-y-[0.25rem] rounded-[1.25rem] bg-white p-[1.25rem] shadow-[0_0_20px_0_rgba(0,0,0,0.1)] sm:min-h-[12rem] md:min-h-[90%] md:w-[33%]'>
+    <div className='relative flex min-h-[15rem] w-full flex-col items-start space-y-[0.25rem] rounded-[1.25rem] bg-white p-[1.25rem] shadow-[0_0_20px_0_rgba(0,0,0,0.1)] sm:min-h-[12rem] md:min-h-[85%] md:w-[33%] lg:min-h-[90%] xl:min-h-[85%]'>
       <p className='cursor-pointer text-[24px] font-bold text-[#2F327D] lg:text-[24px]'>Vật lý 1</p>
       <div className='flex flex-row items-center justify-start space-x-[0.5rem]'>
         {width > 300 && <Icon.LocationTransparent className='max-h-[1.5rem]' />}
