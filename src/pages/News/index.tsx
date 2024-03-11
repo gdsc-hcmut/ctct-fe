120b708b5885b03940dcd8f83638bb2843a25138
dvtdat <tiendat0417@gmail.com>
2024-03-11 15:00:15 +0700

    feat: add template to page

--- src/pages/News/index.tsx
+++ src/pages/News/index.tsx
@@ -5,6 +5,7 @@ const NewsPage = () => {
   return (
     <Page title='Tin tức'>
       <main className='with-nav-height flex flex-col gap-y-5 overflow-hidden overflow-y-auto text-[16px] md:text-[14px] lg:gap-y-10 lg:text-[18px] xl:text-[20px] 2xl:gap-y-[54px] 3xl:gap-y-[60px]'>
+        <div className='mb-16 flex w-full flex-col gap-y-0 px-5 md:gap-y-12 md:px-12 lg:mb-24 lg:gap-y-20 lg:px-24 2xl:mb-32 2xl:gap-y-24 2xl:px-32 3xl:mb-36 3xl:gap-y-28 3xl:px-40'></div>
         <Footer />
       </main>
     </Page>
