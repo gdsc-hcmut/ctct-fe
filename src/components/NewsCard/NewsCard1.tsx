9c8a98a412e18b21570624d727596ea1a12d572a
dvtdat <tiendat0417@gmail.com>
2024-03-14 13:48:47 +0700

    feat: responsive update on newscards

--- src/components/NewsCard/NewsCard1.tsx
+++ src/components/NewsCard/NewsCard1.tsx
@@ -18,18 +18,22 @@ const NewsCard1 = ({ eventSets, isSolidColor, isImageLeft, title }: NewsCardProp
           : 'border-0 border-[#696984] border-opacity-10 bg-white md:border-[1px]'
       }`}
     >
-      <div className='flex w-full flex-col border-b-[1px] border-[#696984] border-opacity-10 pb-[0.5rem]'>
-        <p className='text-start text-[18px] font-semibold text-[#696984] md:text-[14x] xl:text-[20px]'>
+      <div className='flex w-full flex-col border-b-[1px] border-[#4285F4] pb-[0.5rem] md:border-[#696984] md:border-opacity-10'>
+        <p className='text-start text-[18px] font-semibold text-[#4285F4] md:text-[#696984] md:text-[14x] xl:text-[20px]'>
           {title}
         </p>
       </div>
-      <div className={`mt-[1.25rem] flex ${isImageLeft ? 'flex-row' : 'flex-row-reverse'} `}>
+      <div
+        className={`mt-[1.25rem] flex flex-col ${
+          isImageLeft ? 'md:flex-row' : 'md:flex-row-reverse'
+        } `}
+      >
         <div
           className={`flex ${
-            isImageLeft ? 'max-w-[45%]' : 'ml-[1.5rem] w-full max-w-full'
-          } flex-col space-y-[0.75rem]`}
+            isImageLeft ? 'max-w-full md:max-w-[45%]' : 'ml-0 w-full max-w-full md:ml-[1.5rem]'
+          } flex-col space-y-[0.75rem] border-b-[1px] border-dashed border-[#696984] border-opacity-10 pb-[1rem] md:border-0`}
         >
-          <div className='relative block aspect-[2/1] h-auto w-[100%] overflow-hidden'>
+          <div className='relative block aspect-[3/2] h-auto w-[100%] overflow-hidden md:aspect-[2/1]'>
             <LazyLoadImage
               className='z-[1] block aspect-auto rounded-[0.5rem]'
               src={require('../../assets/images/News_1.png')}
@@ -38,36 +42,48 @@ const NewsCard1 = ({ eventSets, isSolidColor, isImageLeft, title }: NewsCardProp
               objectFit='cover'
             />
           </div>
-          <p className='text-start font-semibold text-[12x] text-[#696984] xl:text-[16px]'>
+          <p className='hidden text-start font-semibold text-[12x] text-[#696984] md:block xl:text-[16px]'>
             Độc đáo lớp học cùng tiến, giúp bạn học tốt hơn
           </p>
+          <div className='mt-[1rem] flex min-w-full flex-col md:mt-0 md:hidden md:min-w-[65%] lg:max-w-full xl:max-w-[65%]'>
+            <p className='text-start text-[16px] font-semibold text-black md:text-[14px] md:text-[#696984] 3xl:text-[16px]'>
+              Độc đáo lớp học cùng tiến, giúp bạn học tốt hơn
+            </p>
+            <p className='mt-[0.25rem] text-start text-[14px] leading-6 text-[#696984] xl:leading-7'>
+              Nhiều trường ĐH tuyển chọn sinh viên khá, giỏi làm trợ lý giảng dạy cho giảng viên.
+              Các “giảng viên sinh viên" này tham gia đứng lớp giảng dạy, phụ chấm bài và được nhà
+              trường trả lương.
+            </p>
+          </div>
         </div>
 
         <div
           className={`flex ${
-            isImageLeft ? 'ml-[1.5rem] w-full max-w-full' : 'min-w-[45%]'
+            isImageLeft
+              ? 'ml-0 mt-[1.25rem] w-full max-w-full md:mt-0 md:ml-[1.5rem]'
+              : 'min-w-[45%]'
           } flex-col space-y-[1.5rem]`}
         >
           <div className='flex max-w-full flex-col border-b-[1px] border-dashed border-[#696984] border-opacity-10 pb-[0.5rem]	'>
-            <p className='text-start font-normal text-[12x] text-[#696984] xl:text-[16px] '>
+            <p className='text-start font-medium text-[12x] text-black md:font-normal md:text-[#696984] xl:text-[16px] '>
               Thông tin lớp học tuần 14/2024
             </p>
           </div>
 
           <div className='flex max-w-full flex-col border-b-[1px] border-dashed border-[#696984] border-opacity-10 pb-[0.5rem]	'>
-            <p className='text-start font-normal text-[12x] text-[#696984] xl:text-[16px] '>
+            <p className='text-start font-medium text-[12x] text-black md:font-normal md:text-[#696984] xl:text-[16px] '>
               Thông tin lớp học tuần 15/2024
             </p>
           </div>
 
           <div className='flex max-w-full flex-col border-b-[1px] border-dashed border-[#696984] border-opacity-10 pb-[0.5rem]	'>
-            <p className='text-start font-normal text-[12x] text-[#696984] xl:text-[16px] '>
+            <p className='text-start font-medium text-[12x] text-black md:font-normal md:text-[#696984] xl:text-[16px] '>
               Mở đăng ký thi thử giữa kỳ HK232
             </p>
           </div>
 
           <div className='flex max-w-full flex-col border-b-[1px] border-dashed border-[#696984] border-opacity-10 pb-[0.5rem]	'>
-            <p className='text-start font-normal text-[12x] text-[#696984] xl:text-[16px] '>
+            <p className='text-start font-medium text-[12x] text-black md:font-normal md:text-[#696984] xl:text-[16px] '>
               Lịch thi thử cuối kỳ HK232
             </p>
           </div>
--- src/components/NewsCard/NewsTimetableCard.tsx
+++ src/components/NewsCard/NewsTimetableCard.tsx
@@ -14,13 +14,13 @@ const translateDayOfWeekVietnamese = (dayOfWeek: number) => {
 const NewsTimetableCard = ({ eventSets }: NewsTimetableCardProps) => {
   console.log(eventSets);
   return (
-    <div className='flex w-full flex-col rounded-[0.5rem] border-[1px] border-[#696984] border-opacity-10 bg-white p-[1.25rem]'>
-      <div className='flex w-full flex-col border-b-[1px] border-[#696984] border-opacity-10 pb-[0.5rem]'>
-        <p className='text-start font-semibold text-[14x] text-[#696984] xl:text-[20px]'>
+    <div className='flex w-full flex-col rounded-[0.5rem] border-0 border-[#696984] border-opacity-10 bg-white p-0 md:border-[1px] md:p-[1.25rem]'>
+      <div className='flex w-full flex-col border-b-[1px] border-[#4285F4] pb-[0.5rem] md:border-[#696984] md:border-opacity-10'>
+        <p className='text-start text-[18px] font-semibold text-[#4285F4] md:text-[#696984] md:text-[14x] xl:text-[20px]'>
           LỚP HỌC SẮP TỚI
         </p>
       </div>
-      <div className='grid grid-cols-3 gap-[1rem] pt-[1.25rem]'>
+      <div className='grid grid-cols-2 gap-[0.5rem] pt-[1.25rem] md:grid-cols-3 md:gap-[1rem]'>
         {eventSets.map((event, index) => (
           <div
             key={index}
@@ -32,7 +32,6 @@ const NewsTimetableCard = ({ eventSets }: NewsTimetableCardProps) => {
                 {new Date(event.startedAt).getDate()}/{new Date(event.startedAt).getMonth() + 1}
               </p>
               <div className='flex max-h-full max-w-full flex-row items-center justify-end'>
-                {' '}
                 <Icon.Clock className='aspect-square w-2 fill-[#696984] p-0 lg:w-3 2xl:w-4' />
                 <p className='ml-[0.25rem] text-start font-normal text-[10x] text-[#696984] xl:text-[14px]'>
                   {new Date(event.startedAt).toLocaleTimeString('vi-VN', {
@@ -43,7 +42,7 @@ const NewsTimetableCard = ({ eventSets }: NewsTimetableCardProps) => {
               </div>
             </div>
             <div className='mt-[0.125rem] flex flex-row items-end justify-between'>
-              <p className='text-start font-medium text-[12x] text-[#696984] xl:text-[16px]'>
+              <p className='text-start font-medium text-[12x] text-black md:text-[#696984] xl:text-[16px]'>
                 {event.lhotMetadata.subject.name}
               </p>
             </div>
@@ -55,7 +54,7 @@ const NewsTimetableCard = ({ eventSets }: NewsTimetableCardProps) => {
           </div>
         ))}
       </div>
-      <div className='flex w-full flex-row items-center justify-end'>
+      <div className='mt-[1rem] flex w-full flex-row items-center justify-end md:mt-0'>
         <a
           className='text-start font-normal text-[10x] text-[#696984] underline xl:text-[14px]'
           href='../events/lop-hoc-on-tap'
