c8b546aa4ac8bf95074259e5da8d3c272c20f122
dvtdat <tiendat0417@gmail.com>
2024-04-21 22:04:25 +0700

    feat: reponsive for event item

--- src/components/EventItem/index.tsx
+++ src/components/EventItem/index.tsx
@@ -1,27 +1,34 @@
 import { Link } from 'react-router-dom';
 
 import { LazyLoadImage } from '../';
+import { useWindowDimensions } from '../../hooks';
 import { Event } from '../../types/events';
 interface EventItemProps {
   event: Event;
 }
 
+const CUTOFF_DESCRIPTION_LENGTH = 100;
+const CUTOFF_DESCRIPTION_LENGTH_MOBILE = 20;
+
 const EpochTimeToDateStringVietnamese = (epochTime: number) => {
   const date = new Date(epochTime);
   return `Ngày ${date.getDate()} tháng ${date.getMonth() + 1} năm ${date.getFullYear()}`;
 };
-
 const EventItem = ({ event }: EventItemProps) => {
+  const { width } = useWindowDimensions();
+
   let displayedDescription = event.description.split('\n')[0];
-  if (displayedDescription.length > 100) {
+  const cutoffDescriptionLength =
+    width > 768 ? CUTOFF_DESCRIPTION_LENGTH : CUTOFF_DESCRIPTION_LENGTH_MOBILE;
+  if (displayedDescription.length > cutoffDescriptionLength) {
     const displayedDescriptionArray = displayedDescription.split(' ');
-    displayedDescription = displayedDescriptionArray.slice(0, 100).join(' ');
+    displayedDescription = displayedDescriptionArray.slice(0, cutoffDescriptionLength).join(' ');
   }
   displayedDescription += ' . . .';
 
   return (
     <div className='mb-[2rem] flex flex-col'>
-      <div className='flex flex-row'>
+      <div className='flex flex-col md:flex-row'>
         <div className='flex flex-row items-start justify-center'>
           <div className='aspect-square h-auto w-[8.75rem] rounded-full bg-blue-500'>
             <LazyLoadImage
@@ -33,22 +40,22 @@ const EventItem = ({ event }: EventItemProps) => {
             />
           </div>
         </div>
-        <div className='ml-[1.5rem] flex flex-col lg:ml-[2rem] xl:ml-[2.5rem] 3xl:ml-[3rem]'>
-          <div className='flex flex-row'>
-            <h3 className='font-semibold text-[#696984]'>
+        <div className='ml-0 flex flex-col items-center md:ml-[1.75rem] md:items-start lg:ml-[2rem] xl:ml-[2.5rem] 3xl:ml-[3rem]'>
+          <div className='flex flex-col lg:flex-row'>
+            <h3 className='mt-[0.5rem] text-center font-semibold text-[#696984] md:mt-0 md:text-start'>
               {EpochTimeToDateStringVietnamese(event.startedAt)}
             </h3>
-            <h3 className='ml-[0.75rem] font-normal text-[#696984] lg:ml-[1rem] xl:ml-[1.25rem] 3xl:ml-[1.5rem] '>
+            <h3 className='mt-[0.5rem] text-center font-normal text-[#696984] md:mt-[0.5rem] md:ml-0 md:text-start lg:mt-0 lg:ml-[1rem] xl:ml-[1.25rem] 3xl:ml-[1.5rem]'>
               {event.venue}
             </h3>
           </div>
-          <div className='mt-[0.25rem] flex flex-row'>
-            <h2 className='text-start text-[20px] font-semibold text-[#000000] lg:text-[24px] xl:text-[28px] 2xl:text-[32px]'>
+          <div className='mt-[0.5rem] flex flex-row'>
+            <h2 className='text-center text-[24px] font-semibold text-[#000000] md:text-start md:text-[20px] lg:text-[24px] xl:text-[28px] 2xl:text-[32px]'>
               {event.name.toUpperCase()}
             </h2>
           </div>
-          <div className='mt-[0.25rem] flex flex-row xl:mt-[0.5rem]'>
-            <p className='text-[#696984]'>{displayedDescription}</p>
+          <div className='mt-[0.75rem] flex flex-row xl:mt-[0.5rem]'>
+            <p className='text-center text-[#696984] md:text-start'>{displayedDescription}</p>
           </div>
           <Link
             className='mt-[1rem] flex aspect-[3/1] h-auto w-[10rem] flex-row items-center justify-center rounded-[0.5rem] bg-[#4285F4] hover:opacity-90 xl:mt-[1.5rem]'
--- src/pages/Events/Events/index.tsx
+++ src/pages/Events/Events/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { Footer, LazyLoadImage, EventItem } from '../../../components';
+import { useWindowDimensions } from '../../../hooks';
 import { Page } from '../../../layout';
 import EventService from '../../../service/event.service';
 import useBoundStore from '../../../store';
@@ -8,6 +9,8 @@ import useBoundStore from '../../../store';
 const ONE_DAY_MILLISECOND = 24 * 60 * 60 * 1000;
 
 const EventsPage = () => {
+  const { width } = useWindowDimensions();
+
   const { data: events } = useQuery({
     queryKey: ['events', 'OTHER'],
     queryFn: async () => {
@@ -36,7 +39,7 @@ const EventsPage = () => {
       <main className='flex w-full flex-col px-6 py-5 lg:px-10 lg:py-7 xl:px-20 3xl:px-[100px] 3xl:py-9'>
         <section
           id='ctct-banner'
-          className='relative -mx-6 -mt-5 flex h-[18rem] w-screen flex-row items-center justify-center md:m-0 md:w-full'
+          className='relative -mx-6 -mt-5 flex h-[8rem] w-screen flex-row  items-center justify-center md:m-0 md:h-[10rem] md:w-full lg:h-[18rem]'
         >
           <LazyLoadImage
             src={require('../../../assets/images/SCTTM_6.jpg')}
@@ -51,16 +54,18 @@ const EventsPage = () => {
           </div>
         </section>
 
-        <div className='mt-[1.75rem] flex flex-col justify-center gap-2 lg:mt-[2rem] lg:gap-4 xl:mt-[2.5rem] 2xl:gap-5 3xl:mt-[2.75rem]'>
-          <h2 className='text-start text-[24px] font-semibold text-[#000000] lg:text-[28px] xl:text-[32px] 2xl:text-[36px]'>
-            Sự kiện sắp tới
-          </h2>
-        </div>
+        {width > 768 && (
+          <div className='mt-[1rem] flex flex-col justify-center gap-2 md:mt-[1.25rem] lg:mt-[1.75rem] lg:gap-4 xl:mt-[2.25rem] 2xl:gap-5 3xl:mt-[2.5rem]'>
+            <h2 className='text-start text-[24px] font-semibold text-[#000000] lg:text-[28px] xl:text-[32px] 2xl:text-[36px]'>
+              Sự kiện sắp tới
+            </h2>
+          </div>
+        )}
 
-        <div className='mt-[1.75rem] flex flex-col justify-center gap-2 lg:mt-[2rem] lg:gap-4 xl:mt-[2.5rem] 2xl:gap-5 3xl:mt-[2.75rem]'>
+        <div className='mt-[1.25rem] flex flex-col justify-center gap-2 lg:mt-[1.75rem] lg:gap-4 xl:mt-[2rem] 2xl:gap-5 3xl:mt-[2.25rem]'>
           <div className='relative flex w-full flex-[2] items-center'>
             <input
-              className='flex w-full flex-1 rounded-lg border border-[#CCC] p-1 text-xs font-medium lg:p-3 lg:text-sm 3xl:p-5 3xl:text-base'
+              className='flex w-full flex-1 rounded-lg border border-[#CCC] p-1 text-sm font-medium md:text-base lg:p-3 lg:text-lg 3xl:p-5'
               value={filterName}
               onChange={({ target }) => {
                 setFilterName(target.value);
